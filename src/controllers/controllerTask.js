47d517df321de0207c6a9152979fc1a9a4b39e2c
Leandro Corbalan <leandrocorbalan88@gmail.com>
2020-09-15 21:20:15 -0300

    backend validations

--- src/controllers/controllerTask.js
+++ src/controllers/controllerTask.js
@@ -2,23 +2,47 @@ const Task = require('../models/Task');
 
 module.exports = {
     create: async (req, res) => {
-        let newTask = new Task(req.body);  
-        newTask.userId = req.params.userId;      
-        await newTask.save(err => console.error(err));
-        res.redirect('/');
+
+        const { title, description } = req.body;
+        if (title.length > 2 && description.length > 2){
+            let newTask = new Task(req.body);  
+            newTask.userId = req.params.userId;      
+            await newTask.save(err => console.error(err));
+            res.redirect('/');
+        } else {
+            let error = 'Make segure that the title and the description are longer than 2 caracters';
+            const loggedUser = req.session.user
+            const userTasks = await Task.find({userId: loggedUser._id})
+            res.render('./web/home', {userTasks, error});
+        }
+        
     },
     check: async (req, res) => {
+
         let taskToCheck = await Task.findById(req.params.id);
         taskToCheck.status = !taskToCheck.status
         await taskToCheck.save()
         res.redirect('/');
+
     },
     update: async (req, res) => {
-        await Task.findByIdAndUpdate(req.params.id, req.body);
-        res.redirect('/');
+
+        const { title, description } = req.body;
+        if (title.length > 2 && description.length > 2){
+            await Task.findByIdAndUpdate(req.params.id, req.body);
+            res.redirect('/');
+        } else {
+            let error = 'Make segure that the title and the description are longer than 2 caracters';
+            const loggedUser = req.session.user
+            const userTasks = await Task.find({userId: loggedUser._id})
+            res.render('./web/home', {userTasks, error});
+        }        
+
     },
     destroy: async (req, res) => {
+
         await Task.findByIdAndDelete(req.params.id);
         res.redirect('/');
+
     }
 }
--- src/controllers/controllerUser.js
+++ src/controllers/controllerUser.js
@@ -4,10 +4,17 @@ const User = require('../models/User');
 module.exports = {
     create: async (req, res) =>{
 
-        let user = new User(req.body);
-        user.password = bcryptjs.hashSync(req.body.password, 10);//hashing password
-        await user.save(err => console.error(err))
-        res.redirect('/');
+        const { userName, firstName, lastName, email, password, confirmPassword } = req.body;
+        const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(userName.length > 2 && firstName.length > 2 && lastName.length > 2 && password == confirmPassword && password.length >= 8 && emailRegex.test(email)){
+            let user = new User(req.body);
+            user.password = bcryptjs.hashSync(req.body.password, 10);//hashing password
+            await user.save(err => console.error(err))
+            res.redirect('/');
+        } else {
+            let error = 'There was an error on the registration';
+            res.render('./web/home', { error });
+        }   
 
     },
     singIn: async(req, res) =>{
@@ -19,13 +26,18 @@ module.exports = {
                     {userName: new RegExp(`^${userOrEmail}$`, 'i')},
                     {email: new RegExp(`^${userOrEmail}$`, 'i')}
                 ]
-        })        
-        userToLogin.password = null;
-        req.session.user = userToLogin;
-        if(req.body.rememberme){
-            res.cookie('email', userToLogin.email, { maxAge: 1000 * 60 * 60 * 24 })
-        }
-        res.redirect('/');
+        })   
+        if(userToLogin && bcryptjs.compareSync(req.body.password, userToLogin.password)){
+            userToLogin.password = null;
+            req.session.user = userToLogin;
+            if(req.body.rememberme){
+                res.cookie('email', userToLogin.email, { maxAge: 1000 * 60 * 60 * 24 })
+            }
+            res.redirect('/');
+        } else {
+            let error = 'Non-existent user or invalid password';
+            res.render('./web/home', { error });
+        }        
 
     },
     logOut: (req, res) =>{
